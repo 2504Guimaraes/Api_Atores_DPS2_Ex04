86a4716e3265abac35f589eda5d2dd4884297002
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-06-23 23:19:16 -0300

    classe modelo p/ entidades feita :D

--- src/main/java/com/example/apiAtoresCiro/models/AbstractEntity.java
+++ src/main/java/com/example/apiAtoresCiro/models/AbstractEntity.java
@@ -1,4 +1,38 @@
 package com.example.apiAtoresCiro.models;
 
-public class AbstractEntity {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
