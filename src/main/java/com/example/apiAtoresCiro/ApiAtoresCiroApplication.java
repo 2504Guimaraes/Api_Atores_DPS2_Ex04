5ec8b792bee25e227952cc70f3cab958c2c3943f
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-06-23 22:53:13 -0300

    Projeto Ex. 04 / DPS2-Ex04 criado!

---
+++ src/main/java/com/example/apiAtoresCiro/ApiAtoresCiroApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiAtoresCiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAtoresCiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAtoresCiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiAtoresCiro/ApiAtoresCiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiAtoresCiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAtoresCiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
